8b58b5c7ead8ff9cc125f452eb76f7081bd6a193
WilmerRojas <wilmer7522@gmail.com>
2024-11-13 06:48:33 -0500

    feat :sparkles: working in DB

--- consultas_y_funciones/taller_funciones.sql
+++ consultas_y_funciones/taller_funciones.sql
@@ -76,12 +76,89 @@ select lower(concat_ws('',nombre,  ' ',  apellido1, ' ',  apellido2)) as Nombre_
 select  concat(id,' ' , substring(nif,1, 8) ) as ID_NIF, substring(nif, 9,1) as Letra_Nif from empleado;
 select id, substring(nif,1, 8), substring(nif, 9,1) from empleado;
 
+select id, REGEXP_REPLACE(nif,'[^A-Z]','') as Letras,
+REGEXP_REPLACE(nif,'[^0-9]','') as codigo from empleado;
+
 -- 11. Lista el nombre de cada departamento y el valor del presupuesto actual del que dispone. Para calcular este dato tendrá que restar al valor del
 -- presupuesto inicial (columna presupuesto) los gastos que se han generado (columna gastos). Tenga en cuenta que en algunos casos pueden existir
 -- valores negativos. Utilice un alias apropiado para la nueva columna columna que está calculando.
-create function presupuesto (
+DELIMITER //
+CREATE FUNCTION presupuesto_Fun(presupuesto double, gastos double)
+RETURNS double DETERMINISTIC
+BEGIN
+	RETURN presupuesto - gastos;
+END //
+DELIMITER ;
+
+
+select nombre as Departamento, presupuesto_fun(presupuesto, gastos) as Presupuesto from departamento;
 
 
 -- 12. Lista el nombre de los departamentos y el valor del presupuesto actual ordenado de forma ascendente.
+select nombre, presupuesto_fun(presupuesto, gastos) from departamento order by nombre asc;
+
 -- 13. Lista el nombre de todos los departamentos ordenados de forma ascendente.
+select nombre from departamento order by nombre asc;
+
 -- 14. Lista el nombre de todos los departamentos ordenados de forma descendente.
+select nombre from departamento order by nombre desc;
+
+/*15. Lista los apellidos y el nombre de todos los empleados, ordenados de forma
+alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su nombre.*/
+select apellido1 as Primer_Apellido, apellido2 as Segundo_Apellido, nombre as Nombre from empleado order by 1,2,3 asc;
+
+/*16. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen mayor presupuesto.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento order by 2 desc limit 3;
+
+/*17. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos que tienen menor presupuesto.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento order by 2 asc limit 3;
+
+/*18. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen mayor gasto.*/
+select nombre as Nombre, gastos as Gastos from departamento order by 2 desc limit 2;
+
+/*19. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen menor gasto.*/
+select nombre as Nombre, gastos as Gastos from departamento order by 2 asc limit 2;
+
+/*20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La
+tercera fila se debe incluir en la respuesta. La respuesta debe incluir todas las columnas de la tabla empleado.*/
+select * from empleado limit 5 offset 2;
+select * from empleado limit 2,5;
+
+/*21. Devuelve una lista con el nombre de los departamentos y el presupuesto,
+ de aquellos que tienen un presupuesto mayor o igual a 150000 euros.*/
+ select nombre as Nombre, presupuesto as Presupuesto from departamento where presupuesto >= 150000;
+ 
+/*22. Devuelve una lista con el nombre de los departamentos y el gasto, de aquellos que tienen menos de 5000 euros de gastos.*/
+select nombre as Nombre, gastos as Gastos from departamento where gastos <= 5000;
+
+/*23. Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto entre 100000 y 200000 euros. Sin
+utilizar el operador BETWEEN.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento where presupuesto >= 100000 and presupuesto <= 200000;
+
+/*24. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento where presupuesto <= 100000 or presupuesto >= 200000;
+
+/*25. Devuelve una lista con el nombre de los departamentos que tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento where presupuesto between 100000 and 200000;
+
+/*26. Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.*/
+select nombre as Nombre, presupuesto as Presupuesto from departamento where presupuesto not between 100000 and 200000;
+
+/*27. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean mayores
+que el presupuesto del que disponen.*/
+select nombre, gastos, presupuesto from departamento where gastos > presupuesto;
+
+/*28. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean menores
+que el presupuesto del que disponen.*/
+select nombre, gastos, presupuesto from departamento where gastos < presupuesto;
+
+/*29. Devuelve una lista con el nombre de los departamentos, gastos y presupuesto, de aquellos departamentos donde los gastos sean iguales al
+presupuesto del que disponen.*/
+-- 30. Lista todos los datos de los empleados cuyo segundo apellido sea NULL.
+-- 31. Lista todos los datos de los empleados cuyo segundo apellido no sea NULL.
+-- 32. Lista todos los datos de los empleados cuyo segundo apellido sea López.
+-- 33. Lista todos los datos de los empleados cuyo segundo apellido sea Díaz o Moreno. Sin utilizar el operador IN.
+-- 34. Lista todos los datos de los empleados cuyo segundo apellido
+-- sea Díaz o Moreno. Utilizando el operador IN.
+-- 35. Lista los nombres, apellidos y nif de los empleados que trabajan en el departamento 3.
+-- 36. Lista los nombres, apellidos y nif de los empleados que trabajan en los departamentos 2, 4 o 5.
